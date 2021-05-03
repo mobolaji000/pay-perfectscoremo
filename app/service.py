599bff762cf5a7b95b039d9463bf26916d40aa8e
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2021-05-03 07:08:53 -0500

    comment out checking for existing customers

--- app/service.py
+++ app/service.py
@@ -54,8 +54,8 @@ class StripeInstance():
         pass
 
     def createCustomer(self, clientSetupData):
-
-        existing_customer = DBClient.query.filter_by(email=clientSetupData['email']).order_by(DBClient.date_created.desc()).first() or DBClient.query.filter_by(phone_number=clientSetupData['phone_number']).order_by(DBClient.date_created.desc()).first()
+        #reverse to chcek for existing customer after testing
+        existing_customer = None# DBClient.query.filter_by(email=clientSetupData['email']).order_by(DBClient.date_created.desc()).first() or DBClient.query.filter_by(phone_number=clientSetupData['phone_number']).order_by(DBClient.date_created.desc()).first()
         print("existing customer is ",existing_customer)
         customer = stripe.Customer.retrieve(existing_customer.stripe_customer_id) if existing_customer else stripe.Customer.create(email=clientSetupData['email'],name=clientSetupData['first_name'] + " " + clientSetupData['last_name'],phone=clientSetupData['phone_number'])
         print("new customer is ",customer)
