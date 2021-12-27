eab59dd06962ee1749df8a91359bfda35432849f
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2021-12-27 09:14:32 -0600

    error handling for missing phone numebrs

--- app/views.py
+++ app/views.py
@@ -111,7 +111,7 @@ def client_info(prospect_id):
         try:
             student_data = request.form.to_dict()
             AppDBUtil.createStudentData(student_data)
-            to_numbers = [number for number in [student_data.get('parent_1_phone_number', None), student_data.get('parent_1_phone_number', None), student_data.get('student_phone_number', None)] if number]
+            to_numbers = [number for number in [student_data['parent_1_phone_number'],student_data['parent_1_phone_number'],student_data['student_phone_number']] if number != '']
             SendMessagesToClients.sendGroupSMS(to_numbers=to_numbers, message=student_data['student_first_name'], type='create_group_chat')
             #hold off on sending group emails until you dedcide there is a value add
             #SendMessagesToClients.sendEmail(to_addresses=[student_data['parent_1_email'], student_data['parent_2_email'], student_data['student_email'],'mo@perfectscoremo.com'], message=student_data['student_first_name'], type='create_group_email',subject='Setting Up Group Email')
