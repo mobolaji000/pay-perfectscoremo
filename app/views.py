953b5c48f7bf9cd472ad39099113c10eb1d04baa
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-03-01 10:32:22 -0600

    test datetime3

--- app/views.py
+++ app/views.py
@@ -585,16 +585,12 @@ def start_background_jobs_before_first_request():
 
     scheduler = BackgroundScheduler(timezone='US/Central')
 
-    if os.environ['DEPLOY_REGION'] == 'local':
-        #scheduler.add_job(invoice_payment_background_job, 'cron', second='30')
-        #scheduler.add_job(reminders_background_job,'cron',minute='55')
-        scheduler.add_job(lambda: print("dummy reminders job for local"), 'cron', minute='55')
+    if os.environ['DEPLOY_REGION'] != 'prod':
+    #if os.environ['DEPLOY_REGION'] != 'local':
+        scheduler.add_job(lambda: print("dummy reminders job for local and dev"), 'cron', minute='55')
     else:
         scheduler.add_job(reminders_background_job, 'cron', hour='19', minute='45')
-        #uncomment after Akinyoade payment
-        # scheduler.add_job(reminders_background_job, 'cron', day_of_week='sat', hour='19', minute='45')
         # scheduler.add_job(reminders_background_job, 'cron', day_of_week='sun', hour='19', minute='45')
-
         scheduler.add_job(invoice_payment_background_job, 'cron', hour='23',minute='45')
 
     print("Reminders background job added")
@@ -604,6 +600,7 @@ def start_background_jobs_before_first_request():
     stamp = int(datetime.datetime.now().timestamp())
     date = datetime.datetime.fromtimestamp(stamp)
     print(date)
+    print("timezone info is: ",datetime.datetime.today().astimezone().tzinfo)
 
     scheduler.start()
 
