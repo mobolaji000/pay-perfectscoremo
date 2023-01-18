93ad04fad31cd46e37265354e50dc02adff5e020
mobolaji000 <47364911+mobolaji000@users.noreply.github.com>
2023-01-18 01:02:52 +0000

    populate fields on lead_by_lead page4

--- app/views.py
+++ app/views.py
@@ -165,9 +165,10 @@ def client_info(prospect_id):
 @server.route('/lead_info_by_lead',defaults={'lead_id': None}, methods=['GET','POST'])
 @server.route('/lead_info_by_lead/<lead_id>', methods=['GET','POST'])
 def lead_info_by_lead(lead_id):
+    logger.debug("lead_info_by_lead mehtod is: {}".format(request.method))
     if request.method == 'GET':
         try:
-            lead_info_contents = AppDBUtil.getLeadInfo(lead_id)
+            lead_info_contents = AppDBUtil.getLeadInfo(lead_id)[0]
             logger.debug("lead_id in get is: {}".format(lead_id))
             return render_template('lead_info_by_lead.html',
                                    lead_id=lead_info_contents['lead_id'],
