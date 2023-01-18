87f1ae5b74a8b21fdd9fc6f1fedcb10fdf4e4aa4
mobolaji000 <47364911+mobolaji000@users.noreply.github.com>
2023-01-18 01:33:10 +0000

    populate fields on lead_by_lead page8

--- app/dbUtil.py
+++ app/dbUtil.py
@@ -597,7 +597,7 @@ class AppDBUtil():
                 lead['date_created'] = info.date_created.strftime("%m/%d/%Y")
                 lead['completed_appointment'] = info.completed_appointment
                 lead['grade_level'] = info.grade_level
-                lead['recent_scores'] = info.recent_scores
+                lead['recent_test_score'] = info.recent_test_score
 
                 search_results.append(lead)
             logger.info("search results are {}".format(search_results))
