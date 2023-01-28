f81a8717049b3537fc65447e78e77b693c085aa1
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2023-01-28 13:49:33 -0600

    first test of recurring payments4

--- app/dbUtil.py
+++ app/dbUtil.py
@@ -705,7 +705,7 @@ class AppDBUtil():
         return transaction_ids
 
     @classmethod
-    def getAllLeads(cls):#
+    def getAllLeads(cls):
         allLeads = Lead.query.order_by(Lead.date_created.desc()).all()
         return allLeads
 
