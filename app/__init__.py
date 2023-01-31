c6c408acefd3d35189aaac1ae89361509da18929
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2023-01-30 18:24:22 -0600

    debugging failure on digital ocean5

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,3 @@
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -15,22 +6,10 @@ from flask_bootstrap import Bootstrap
 
 server = Flask(__name__)
 server.config.from_object(Config)
-db = SQLAlchemy(server)
-#db = SQLAlchemy(server,engine_options={"pool_pre_ping": True},session_options={'expire_on_commit': False})
+db = SQLAlchemy(server,engine_options={"pool_pre_ping": True},session_options={'expire_on_commit': False})
 from app import views, models
 migrate = Migrate(server, db)
 bootstrap = Bootstrap(server)
-
-
-#ADDED TO TURN DOUBLE FLASK LOGGING OFF FLASK LOCAL
-from flask.logging import default_handler
-server.logger.removeHandler(default_handler)
-server.logger.handlers.clear()
-
-
-
-
-
 
 
 
