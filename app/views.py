3092aa7a3c84ab6c2b17f3a7eed02f448611fafb
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2023-02-11 12:48:04 -0600

    set up cron for test2

--- app/views.py
+++ app/views.py
@@ -972,10 +972,10 @@ def start_background_jobs_before_first_request():
     elif os.environ['DEPLOY_REGION'] == 'dev':
         scheduler.add_job(remind_client_about_invoice_background_job, 'cron', day_of_week='0-6/2', hour='16-16', minute='55-55', start_date=datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(days=1), '%Y-%m-%d'))
         scheduler.add_job(remind_lead_about_appointment_background_job, 'cron', hour='22', minute='5')
-        scheduler.add_job(restart_paused_payments_background_job, 'interval', minutes=1)
+        scheduler.add_job(restart_paused_payments_background_job, 'interval', minutes=111)
         scheduler.add_job(setup_recurring_payments_due_today_background_job, 'interval', minutes=3)
-        scheduler.add_job(pay_invoice_background_job,'interval', miuntes=5)
-        scheduler.add_job(notify_mo_to_modify_lead_appointment_completion_status_background_job, 'interval', hours=1)
+        scheduler.add_job(pay_invoice_background_job,'interval', miuntes=115)
+        scheduler.add_job(notify_mo_to_modify_lead_appointment_completion_status_background_job, 'interval', hours=111)
         logger.info("all dev background jobs added")
 
 
